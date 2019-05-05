93d8b8c9657db7a525c4cea719ae31f48f824454
Steve Thompson <sthmpsn@gmail.com>
2019-05-05 11:58:53 -0500

    formatted the book information as desired.  Need to implement the "Save" option next

--- client/src/components/Book/Book.js
+++ client/src/components/Book/Book.js
@@ -9,30 +9,30 @@ import Button from "react-bootstrap/Button";
 function Book(props) {
 
     return (
-        <Row>
+        <Row className="my-4 border border-secondary rounded">
             <Col>
-            <Row className="justify-content-between">
-                <Col lg={5}>
-                    <ListGroup className="h6">
-                        <ListGroup.Item className="border-0">{props.book.title}</ListGroup.Item>
-                        <ListGroup.Item className="border-0">Written By: {props.book.authors}</ListGroup.Item>
-                    </ListGroup>
-                </Col>
-                <Col lg={3}>
-                    <Button className="mx-2" href={props.book.link} target="_blank">View</Button>
-                    <Button className="mx-2">Save</Button>
-                </Col>
-            </Row>
-            <Row>
-                <Col lg={3}>
-                    <a href={props.book.link} target="_blank">
-                        <Image src={props.book.imageLink} thumbnail fluid/>
-                    </a>
-                </Col>
-                <Col lg={9} className="text-center">
-                        {props.book.desc}
-                </Col>
-            </Row>
+                <Row className="justify-content-between">
+                    <Col lg={5} className="mt-3">
+                        <ListGroup>
+                            <ListGroup.Item className="border-0 py-1 h5 pl-1">{props.book.title}</ListGroup.Item>
+                            <ListGroup.Item className="border-0 py-1 h6 pl-1">Written By: {props.book.authors}</ListGroup.Item>
+                        </ListGroup>
+                    </Col>
+                    <Col lg={3} className="mt-3">
+                        <Button className="mx-2" href={props.book.link} target="_blank">View</Button>
+                        <Button className="mx-2">Save</Button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col lg={3}>
+                        <a href={props.book.link} target="_blank">
+                            <Image src={props.book.imageLink} thumbnail fluid/>
+                        </a>
+                    </Col>
+                    <Col lg={9} className="book-desc py-2">
+                            {props.book.desc}
+                    </Col>
+                </Row>
             </Col>
         </Row>
     );
