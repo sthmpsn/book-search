2c8ac703781dbdcceef68696c70443945fad4d66
Steve Thompson <sthmpsn@gmail.com>
2019-05-06 11:57:20 -0500

    added react font-awesome icons and google books color formatting

--- client/src/components/NavBoot/NavBoot.js
+++ client/src/components/NavBoot/NavBoot.js
@@ -1,14 +1,26 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import "./navboot.css";
 
 function NavBoot() {
     return (
-        <Navbar>
-            <Navbar.Brand href="https://books.google.com/" target="_blank">Google <span className="font-weight-bold"><span className="text-primary">B</span><span className="text-danger font-weight-bold">o</span><span className="text-warning">o</span><span className="text-success">k</span><span className="text-danger">s</span></span></Navbar.Brand>
-            <Nav.Link className="px-3" href="/">Search</Nav.Link>
-            <Nav.Link className="px-3" href="/saved">Saved</Nav.Link>
-        </Navbar>
+        <span>
+            <Navbar>
+                <Navbar.Brand href="https://books.google.com/" target="_blank">G<i class="fab fa-react"></i><i class="fab fa-react"></i>gle <span className="font-weight-bold"><span className="text-primary">B</span><span className="text-danger font-weight-bold">o</span><span className="text-warning">o</span><span className="text-success">k</span><span className="text-danger">s</span></span></Navbar.Brand>
+                <Nav.Link className="px-3" href="/">Search</Nav.Link>
+                <Nav.Link className="px-3" href="/saved">Saved</Nav.Link>
+            </Navbar>
+            <Jumbotron className="text-center my-3">
+                <h1>
+                    <span className="font-weight-bold text-primary">G<i class="fab fa-react text-danger"></i><i class="fab fa-react text-warning"></i><span className="text-primary">g</span><span className="text-success">l</span><span className="text-danger">e </span>
+                    <i class="fas fa-book mx-3"></i>
+                    <span className="text-primary">B</span><span className="text-danger font-weight-bold">o</span><span className="text-warning">o</span><span className="text-success">k</span><span className="text-danger">s</span></span>
+                </h1>
+                <h2>Search for and Save Books of Interest</h2>
+            </Jumbotron>
+        </span>
     );
 }
 
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -2,12 +2,10 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Jumbotron from "react-bootstrap/Jumbotron";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Book from "../components/Book/Book";
 import API from "../utils/API";
-import "./index.css";
 
 
 class Search extends React.Component {
@@ -35,10 +33,7 @@ class Search extends React.Component {
     render() {
         return (
             <Container className="fluid">
-                <Jumbotron className="text-center my-3">
-                    <h1>(React) Google <span className="font-weight-bold"><span className="text-primary">B</span><span className="text-danger font-weight-bold">o</span><span className="text-warning">o</span><span className="text-success">k</span><span className="text-danger">s</span></span></h1>
-                    <h2>Search for and Save Books of Interest</h2>
-                </Jumbotron>
+
                 <Row>
                     <Col>
                         <Form>
